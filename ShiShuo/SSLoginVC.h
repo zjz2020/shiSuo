f74259edf8fe539db4fd529a271a657d1439964e
zhang junze <602127606@qq.com>
2016-07-14 13:56:25 -0700

    注册界面

---
+++ ShiShuo/SSLoginVC.h
@@ -0,0 +1,13 @@
+//
+//  SSLoginVC.h
+//  ShiShuo
+//
+//  Created by 张君泽 on 16/7/14.
+//  Copyright © 2016年 CloudEducation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSLoginVC : UIViewController
+
+@end
---
+++ ShiShuo/SSRgisterVC.m
@@ -0,0 +1,37 @@
+//
+//  SSRgisterVC.m
+//  ShiShuo
+//
+//  Created by 张君泽 on 16/7/14.
+//  Copyright © 2016年 CloudEducation. All rights reserved.
+//
+
+#import "SSRgisterVC.h"
+
+@interface SSRgisterVC ()
+
+@end
+
+@implementation SSRgisterVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
